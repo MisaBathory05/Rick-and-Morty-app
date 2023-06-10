c9071de568927f175667774e1163f701cfaed4ec
MisaBathory05 <tmisa05@gmail.com>
2023-06-10 15:12:14 -0500

    Faltante

--- src/components/About/About.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {styles} from "./About.module.css";
-
-const About = () => {
-    return (
-        <div>Esto es un about, no se para que sirva pero funcionará en algún momento</div>
-    )
-}
-export default About;
--- src/components/Helpers/Routes.helper.js
+++
@@ -1,9 +0,0 @@
-const PathRoutes={
-    HOME:'/home',
-    ABOUT:'/about',
-    DETAIL:'/detail/:id',
-    FAVORITES:'/favorites/:id',
-    FORM: '/',
-}
-
-export default PathRoutes;
